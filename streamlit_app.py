f64737a686f3ca64a01b4de835e4a422e0cc9894
João Marcos Coutinho Nunes <64987085+markinh00@users.noreply.github.com>
2026-05-01 16:39:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json(), use_container_width=True)
-        
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(smoothiefroot_response.json(), data=use_container_width=True)      
